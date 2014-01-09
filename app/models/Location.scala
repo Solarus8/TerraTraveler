f2983b95d01c10d756dc8959b810100cc26dd7ab
GitOutATown <richard@aqume.com>
2014-01-08 21:44:23 -0800

    Removed commented out previous Location.create method`

--- app/models/Location.scala
+++ app/models/Location.scala
@@ -47,56 +47,7 @@ object Location {
 		  	  	Location(id, city, country, addr1, addr2, addr3, postalCode, lat, lon, alt, desc, url)
 		}
 	}
-	
-	/*def create(loc: Location): Pk[Long] = {
-		DB.withConnection { implicit connection =>
-		    println("Location.create - inside DB.withConnection")
-	      	SQL(
-	      		"""
-	      	    insert into location (
-	      	        city, 
-	      	        country, 
-	      	        address_1, 
-	      	        address_2, 
-	      	        address_3, 
-	      	        postal_code, 
-	      			geoloc,
-	      	        altitude, 
-	      	        "desc", 
-	      	        url
-	      	    ) values (
-      				{city}, 
-	      	        {country}, 
-	      	        {addr1}, 
-	      	        {addr2}, 
-	      	        {addr3},
-	      	        {postalCode}, 
-	      	        ST_GeographyFromText('SRID=4326;POINT({lat} {lon})'),
-	      	        {alt}, 
-	      	        {desc}, 
-	      	        {url}
-      			)
-	      	    """
-	      	).on(
-	      	      'city   		-> loc.city,
-	      	      'country		-> loc.country,
-	      	      'addr1		-> loc.addr1,
-	      	      'addr2		-> loc.addr2,
-	      	      'addr3		-> loc.addr3,
-	      	      'postalCode	-> loc.postalCode,
-	      	      'lat			-> loc.lat,
-	      	      'lon			-> loc.lon,
-	      	      'alt			-> loc.alt,
-	      	      'desc			-> loc.desc,
-	      	      'url			-> loc.url
-	      	).executeInsert()
-    	} match {
-    	    case null => { throw new Exception("SQL Error - GOT NULL.")}
-	        case Some(long) => new Id[Long](long) // The Primary Key
-	        case _          => throw new Exception("SQL Error - Did not insert Location.")
-    	}
-	}*/ // End - create
-	
+		
 	def create(loc: Location): Pk[Long] = {
 		DB.withConnection { implicit connection =>
 		    println("Location.create - inside DB.withConnection")
