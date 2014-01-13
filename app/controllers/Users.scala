1de904d92f70a0886477c00f2cc76dc34239b2e3
GitOutATown <richard@aqume.com>
2014-01-13 10:03:40 -0800

    In process of implementing createUserProfile

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import java.util.Date
+
 import models._
 import views._
 import anorm.NotAssigned
@@ -16,56 +18,114 @@ import play.api.data.format.Formats._
 object Users extends Controller {
 	
 	def createUser = Action { implicit request =>
-	    render {
-	        case Accepts.Json() => {
-	            request.body.asJson.map { json =>
-	                
-	                println("Users.createUser - json: " + json)
-	                
-			        val userName 	= (json \ "userName").validate[String]
-			        println("Users.createUser - userName: " + userName)
-			        val email    	= (json \ "email").validate[String]
-			        println("Users.createUser - email: " + email)
-			        val password 	= (json \ "password").validate[String]
-			        println("Users.createUser - password: " + password)
-			        val role 	 	= (json \ "role").validate[String]
-			        println("Users.createUser - role: " + role)
-			        val latitude	= (json \ "latitude").validate[Double]
-			        println("Users.createUser - latitude: " + latitude)
-			        val longitude	= (json \ "longitude").validate[Double]
-			        println("Users.createUser - longitude: " + longitude)
-			        
-			        val newUserLoc	= Location(NotAssigned, null, null, null, null, null, null, latitude.get, longitude.get, null, null, null)
-			        println("Users.createUser - newUserLoc: " + newUserLoc)
-			        val newLocPK = Location.create(newUserLoc)
-			        println("Users.createUser - newLocPK: " + newLocPK)
-			        			        
-			        val newUser  = User(NotAssigned, null, null, null, userName.get, email.get, password.get, role.get, newLocPK.get)
-			        val newUserPK = User.create(newUser)
-			        			        
-			        val persistedUser = User.findById(newUserPK.get)
-			        			        
-			        persistedUser match {
-			            case Some(persistedUser) => {
-			                val jsonResp = Json.obj( "user" -> {
-			                    Json.obj(
-				                    "id"		 -> persistedUser.id.get,
-					    	  	    "userName"   -> persistedUser.userName,
-					        	    "email"      -> persistedUser.email,
-					        	    "password" 	 -> persistedUser.password,
-					        	    "role"	     -> persistedUser.role,
-					        	    "primaryLoc" -> persistedUser.primaryLoc
-					        	)
-				            })
-				            Ok(jsonResp)
-			            }
-			            case None => BadRequest("User not found")
-			        }
-				}.getOrElse {
-					BadRequest("Expecting Json data")
-				}
-	        } // End - case Accpts.Json()
-	    }
+	    request.body.asJson.map { json =>
+	        println("Users.createUser - json: " + json)
+	        
+	        val userName 	= (json \ "userName").validate[String]
+	        //println("Users.createUser - userName: " + userName)
+	        val email    	= (json \ "email").validate[String]
+	        //println("Users.createUser - email: " + email)
+	        val password 	= (json \ "password").validate[String]
+	        //println("Users.createUser - password: " + password)
+	        val role 	 	= (json \ "role").validate[String]
+	        //println("Users.createUser - role: " + role)
+	        val latitude	= (json \ "latitude").validate[Double]
+	        //println("Users.createUser - latitude: " + latitude)
+	        val longitude	= (json \ "longitude").validate[Double]
+	        //println("Users.createUser - longitude: " + longitude)
+	        
+	        val newUserLoc	= Location(NotAssigned, null, null, null, null, null, null, latitude.get, longitude.get, null, null, null)
+	        //println("Users.createUser - newUserLoc: " + newUserLoc)
+	        val newLocPK = Location.create(newUserLoc)
+	        // println("Users.createUser - newLocPK: " + newLocPK)
+	        			        
+	        val newUser  = User(NotAssigned, null, null, null, userName.get, email.get, password.get, role.get, newLocPK.get)
+	        val newUserPK = User.create(newUser)
+	        			        
+	        val persistedUser = User.findById(newUserPK.get)
+	        			        
+	        persistedUser match {
+	            case Some(persistedUser) => {
+	                val jsonResp = Json.obj( "user" -> {
+	                    Json.obj(
+		                    "id"		 -> persistedUser.id.get,
+			    	  	    "userName"   -> persistedUser.userName,
+			        	    "email"      -> persistedUser.email,
+			        	    "password" 	 -> persistedUser.password,
+			        	    "role"	     -> persistedUser.role,
+			        	    "primaryLoc" -> persistedUser.primaryLoc
+			        	)
+		            })
+		            Ok(jsonResp)
+	            }
+	            case None => BadRequest("User not found")
+	        }
+		}.getOrElse {
+			BadRequest("Expecting Json data")
+		}
+	}
+	
+	def createUserProfile = Action { implicit request =>
+	    println("Users.createUserProfile - TOP")
+	    request.body.asJson.map { json =>
+	        println("Users.createUserProfile - json: " + json)
+	        
+	        val userId			= (json \ "userId").validate[Long]
+	        println("Users.createUserProfile - userId" + userId)
+			val firstName		= (json \ "firstName").validate[Option[String]]
+	        println("Users.createUserProfile - firstName" + firstName)
+			val lastName		= (json \ "lastName").validate[Option[String]]
+	        println("Users.createUserProfile - lastName" + lastName)
+			val gender			= (json \ "gender").validate[Option[String]]
+	        println("Users.createUserProfile - gender" + gender)
+			val birthdate		= (json \ "birthdate").validate[Option[Date]]
+	        println("Users.createUserProfile - birthdate" + birthdate)
+			val nationality		= (json \ "nationality").validate[Option[String]]
+	        println("Users.createUserProfile - nationality" + nationality)
+			val portraitUrl		= (json \ "portraitUrl").validate[Option[String]]
+	        println("Users.createUserProfile - portraitUrl" + portraitUrl)
+			val bio				= (json \ "bio").validate[Option[String]]
+	        println("Users.createUserProfile - bio" + bio)
+			val story			= (json \ "story").validate[Option[String]]
+	        println("Users.createUserProfile - story" + story)
+	        
+	        val newProfile = UserProfile(
+                NotAssigned, 
+                userId.get, 
+                firstName.get, 
+                lastName.get, 
+                gender.get, 
+                birthdate.get, 
+                nationality.get, 
+                portraitUrl.get, 
+                bio.get, 
+                story.get
+            )
+            val newUserProfilePK = UserProfile.create(newProfile)
+            val persistedUserProfile = UserProfile.findById(newUserProfilePK.get)
+            persistedUserProfile match {
+	            case Some(persistedUserProfile) => {
+	                val jsonResp = Json.obj( "userProfile" -> {
+	                    Json.obj(
+							"id"    	 	-> persistedUserProfile.id.get,
+							"userId" 		-> persistedUserProfile.userId,
+							"firstName" 	-> persistedUserProfile.firstName,
+							"lastName"		-> persistedUserProfile.lastName,
+							"gender" 		-> persistedUserProfile.gender,
+							"birthdate" 	-> persistedUserProfile.birthdate,
+							"nationality" 	-> persistedUserProfile.nationality,
+							"portraitUrl" 	-> persistedUserProfile.portraitUrl,
+							"bio"			-> persistedUserProfile.bio,
+							"story"			-> persistedUserProfile.story
+			        	)
+		            })
+		            Ok(jsonResp)
+	            }
+	            case None => BadRequest("UserProfile not found")
+	        }
+		}.getOrElse {
+			BadRequest("Expecting Json data")
+		}
 	}
 		
 	def allUsers = Action { implicit request =>
@@ -124,28 +184,20 @@ object Users extends Controller {
 		val up = UserProfile.findProfile(userId) 
 		up match {
 		    case Some(persistedProfile) => {
-		        render {
-		            case Accepts.Json() => {
-				        val profile = Json.obj(
-					        "profile" -> Json.obj(
-					            "id" 			-> persistedProfile.id.get,
-					            "userId"		-> persistedProfile.userId,
-					            "firstName"		-> persistedProfile.firstName,
-					            "lastName"		-> persistedProfile.lastName,
-					            "gender"		-> persistedProfile.gender,
-					            "birthdate"		-> persistedProfile.birthdate,
-					            "nationality"	-> persistedProfile.portraitUrl,
-					            "bio"			-> persistedProfile.bio,
-					            "story"			-> persistedProfile.story
-					         )
-				         )
-				         Ok(profile)
-		            }
-		        }
-		    }
-		    
-		    case _ => render {
-		        case Accepts.Json() => NotFound
+		        val profile = Json.obj(
+			        "profile" -> Json.obj(
+			            "id" 			-> persistedProfile.id.get,
+			            "userId"		-> persistedProfile.userId,
+			            "firstName"		-> persistedProfile.firstName,
+			            "lastName"		-> persistedProfile.lastName,
+			            "gender"		-> persistedProfile.gender,
+			            "birthdate"		-> persistedProfile.birthdate,
+			            "nationality"	-> persistedProfile.portraitUrl,
+			            "bio"			-> persistedProfile.bio,
+			            "story"			-> persistedProfile.story
+			         )
+		         )
+		         Ok(profile)
 		    }
 		}
 	}
--- app/models/UserProfile.scala
+++ app/models/UserProfile.scala
@@ -44,6 +44,43 @@ object UserProfile {
 	    }
 	}
 	
+	def create(profile: UserProfile): Pk[Long] = {
+	    println("UserProfile.create - TOP")
+		DB.withConnection { implicit connection =>
+	      	val sql = SQL(
+	      		"""
+      			insert into user_profile(
+	      	        user_id, first_name, last_name, gender, birthdate, nationality, portrait_url, bio, story
+	      	    ) 
+	      	    values (
+	      	        {userId}, {firstName}, {lastName}, {gender}, {birthdate}, 
+	      	        	{nationality}, {portraitUrl}, {bio}, {story}
+	      	    """
+      	    ).on (
+      	        'userId			-> profile.userId,
+      	        'firstName		-> profile.firstName,
+      	        'lastName		-> profile.lastName,
+      	        'gender			-> profile.gender,
+      	        'birthdate		-> profile.birthdate,
+      	        'nationality	-> profile.nationality,
+      	        'portraitUrl	-> profile.portraitUrl,
+      	        'bio			-> profile.bio,
+      	        'story			-> profile.story
+      	    )
+      	    println("UserProfile.create - sql: " + sql)
+      	    sql.executeInsert()
+		} match {
+	        case Some(long) => {
+	            println("UserProfile.create - Some(long): " + long)
+	            new Id[Long](long) // The Primary Key
+	        }
+	        case _       => {
+	            println("UserProfile.create - None")
+	            throw new Exception("SQL Error - Did not insert UserProfile")
+	        }
+    	}
+	}
+	
 	/**
 	 * Retrieve a UserProfile from user_id.
 	 */
@@ -56,4 +93,23 @@ object UserProfile {
 			).as(UserProfile.simple.singleOpt)
 	    }
 	}
+	
+	/**
+	 * Retrieve a UserProfile by id.
+	 */
+	def findById(id: Long): Option[UserProfile] = {
+		DB.withConnection { implicit connection =>
+	      	SQL("""
+	      	    select * from user_profile where id = {id}
+	      	    """).on(
+	  			'id -> id
+			).as(UserProfile.simple.singleOpt)
+	    }
+	}
 }
+
+
+
+
+
+
