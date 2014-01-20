176344635cc8f86d9d539a797eba5bf02005a180
GitOutATown <richard@aqume.com>
2014-01-19 18:52:56 -0800

    Added diagnostic comment

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,8 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Terra Traveler app is ready."))
+      println("Application.index - TOP")
+	  Ok(views.html.index("Terra Traveler app is ready."))
   }
 
 }
